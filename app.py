6ac5e60ade805848160645dc444bff24f58f67ac
woojingg <k53942068@gmail.com>
2025-09-23 10:52:15 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pandas as pd
 import joblib
 import streamlit as st
 
+st.set_page_config(page_title="IVE Korea 광고 효율 예측기", layout="wide")
 
 # ✅ 캐싱 (모델은 실행할 때마다 다시 안 불러옴)
 @st.cache_resource
@@ -11,11 +12,15 @@ def load_model():
 model = load_model()
 
 # ===== 헤더 (로고 + 제목 일자 정렬) =====
-col1, col2 = st.columns([1,8])
-with col1:
-    st.image("아이브로고.png", width=100)   # GitHub에 있는 경로
-with col2:
-    st.markdown("<h1 style='margin-top: 25px;'>아이브 광고 효율 예측기</h1>", unsafe_allow_html=True)
+st.markdown(
+    """
+    <div style="display: flex; align-items: center; justify-content: center;">
+        <img src="아이브로고.png" width="100" style="margin-right: 20px;">
+        <h1 style="margin: 0;">아이브 광고 효율 예측기</h1>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 
 
 
@@ -42,6 +47,7 @@ if st.button("예측하기"):
         st.dataframe(result_df, use_container_width=True)
     except Exception as e:
         st.error(f"입력 오류: {e}")
+
 
 
 
